e44c902136d39f9b768fd812f57061653bf68e50
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-05-14 15:19:56 +0800

    setting-admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,10 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_protected :is_admin
   # attr_accessible :title, :body
+
+  def is_admin?
+  	is_admin
+  end
 end
