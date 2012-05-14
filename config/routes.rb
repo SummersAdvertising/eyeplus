bf31ec8932891f916c346cbdbd6c8bd4f82bc478
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-05-14 11:56:41 +0800

    setting-devise-as-membership-manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 EyePlus02::Application.routes.draw do
+  devise_for :users
+
   resources :posts
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ EyePlus02::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
