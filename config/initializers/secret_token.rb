006f777bfde346476c7b5019339999b169b095eb
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-05-14 11:28:00 +0800

    track all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyePlus02::Application.config.secret_token = 'e613a22443c82303f3f0559aff25d6649bd7d22a0f26dfe87c5c6617a957b10051107ddb48b25e566591259ffe19d0cd416978f21b96cd7c0e190c459aa5cf9b'
