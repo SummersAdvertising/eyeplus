c93826878fffef162ca3155f1773549f3a64221e
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-05-15 11:07:54 +0800

    setting-author-for-question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   attr_accessible :content, :email, :is_private, :title
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515025053) do
+ActiveRecord::Schema.define(:version => 20120515030125) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120515025053) do
     t.boolean  "is_private"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
