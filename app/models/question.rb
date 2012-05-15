48d0d1ab40748e3924bbd53f5bd7ab8a6791de09
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-05-15 12:15:57 +0800

    setting-delete-answer-operation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   attr_accessible :content, :email, :is_private, :title
   belongs_to :user
-  has_many :answers
+  has_many :answers, :dependent => :destroy
 end
