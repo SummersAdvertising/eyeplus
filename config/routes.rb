d1d815a587ed7a586c1d3821786aa4013c74b3a4
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-05-15 10:56:02 +0800

    setting-question

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 EyePlus02::Application.routes.draw do
+  resources :questions
+
   devise_for :users
   
   namespace :admin do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120514071459) do
+ActiveRecord::Schema.define(:version => 20120515025053) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(:version => 20120514071459) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+  end
+
+  create_table "questions", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.string   "email"
+    t.boolean  "is_private"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
