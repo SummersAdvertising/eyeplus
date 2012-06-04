4f399a99a0adfa29aa82309bf4e2e2ef3ea9cc89
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-04 13:35:27 +0800

    no need to set up apllication level controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,33 +8,4 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
-
-  # default facebook tag values
-  def get_default_facebook_tag_title
-    @default_facebook_tag_title = DefaultFacebook.first.title
-  end
-
-  def get_default_facebook_tag_description
-    @default_facebook_tag_description = "聚英視光眼科診所"
-  end
-
-  def get_default_facebook_tag_image_url
-    @default_facebook_tag_image_url = "www.google.com" 
-  end
-
-  def get_default_facebook_tag_site_type
-    @default_facebook_tag_site_type = "company"
-  end
-
-  def get_default_facebook_tag_url
-    @default_facebook_tag_url = "www.google.com"
-  end
-
-  def get_default_facebook_tag_site_name
-    @default_facebook_tag_site_name = "聚英視光眼科診所"
-  end
-
-  def get_default_facebook_tag_admins
-    @default_facebook_tag_admins = "123456789"
-  end
 end
