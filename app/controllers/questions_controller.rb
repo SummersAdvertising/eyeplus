c488512c98a07d7f7d0554451844816c5f4a1d3b
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-05 15:36:31 +0800

    only create question is allowed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,10 +33,10 @@ class QuestionsController < ApplicationController
   end
 
   # GET /questions/1/edit
-  def edit
-    @question = Question.find(params[:id])
-    # @question = current_user.questions.find(params[:id])
-  end
+  # def edit
+  #   @question = Question.find(params[:id])
+  #   # @question = current_user.questions.find(params[:id])
+  # end
 
   # POST /questions
   # POST /questions.json
@@ -58,30 +58,30 @@ class QuestionsController < ApplicationController
 
   # PUT /questions/1
   # PUT /questions/1.json
-  def update
-    @question = Question.find(params[:id])
-    # @question = current_user.questions.find(params[:id])
+  # def update
+  #   @question = Question.find(params[:id])
+  #   # @question = current_user.questions.find(params[:id])
 
-    respond_to do |format|
-      if @question.update_attributes(params[:question])
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @question.update_attributes(params[:question])
+  #       format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @question.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /questions/1
   # DELETE /questions/1.json
-  def destroy
-    @question = Question.find(params[:id])
-    @question.destroy
+  # def destroy
+  #   @question = Question.find(params[:id])
+  #   @question.destroy
 
-    respond_to do |format|
-      format.html { redirect_to questions_url }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to questions_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 end
