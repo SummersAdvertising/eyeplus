ff865823cd9fd389c4dfeac7218d2ce96627d193
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-05 17:08:34 +0800

    add user name to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120604082701) do
+ActiveRecord::Schema.define(:version => 20120605085522) do
 
   create_table "answers", :force => true do |t|
     t.text     "content"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(:version => 20120604082701) do
     t.datetime "created_at",                                               :null => false
     t.datetime "updated_at",                                               :null => false
     t.boolean  "is_admin",                              :default => false
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
