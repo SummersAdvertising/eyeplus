89f90847f4596177db277c455aa8f99ef7fe1da7
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-07 17:01:36 +0800

    LinNode

---
+++ config/deploy/production.rb
@@ -0,0 +1,36 @@
+set :application, "eyeplus"
+set :domain, "li406-49.members.linode.com"
+set :repository,  "git@github.com:SummersAdvertising/eyeplus.git"
+set :deploy_to, "/var/spool/RoR-Projects/eyeplus"
+
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+
+set :deploy_via, :remote_cache
+set :deploy_env, "production"
+set :rails_env, "production"
+set :scm, :git
+set :branch, "master"
+set :scm_verbose, true
+set :use_sudo, false
+
+set :user, "apps"
+set :password, "1qaz2wsx"
+set :group, "webs"
+# set :group, "apps"
+
+default_environment["PATH"] = "/opt/ree/bin:/usr/local/bin:/usr/bin:/bin:/usr/games"
+
+namespace :deploy do
+  desc "restart"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+desc "Create database.yml and asset packages for production"
+after("deploy:update_code") do
+  db_config = "#{shared_path}/config/database.yml.production"
+  run "cp #{db_config} #{release_path}/config/database.yml"
+end
