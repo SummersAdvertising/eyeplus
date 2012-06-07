de5fca63dafed28b25ec905b40d1262be79800a7
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-07 13:24:39 +0800

    linnode

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,9 @@ require 'bundler/capistrano' #Using bundler with Capistrano
 
 set :stages, %w(staging production)
 set :default_stage, "production"
+
+desc "precompile the assets"
+task :precompile_assets, :roles => :web, :except => { :no_release => true } do
+	run "cd #{current_path}; rm -rf public/assets/*"
+	run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+end
