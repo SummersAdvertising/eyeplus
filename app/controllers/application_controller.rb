522f47e90eb305661112201617dcb56c76a071de
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-11 12:07:23 +0800

    fix-facebook-meta-image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,28 +11,16 @@ class ApplicationController < ActionController::Base
 
   def getFacebook(url)
     @facebook = Facebook.where(:url => url).first
-    if @facebook.nil?
-      @facebook = Facebook.new
-      @defaultFacebook = DefaultFacebook.first
-      @facebook.title = @defaultFacebook.title
-      @facebook.description = @defaultFacebook.description
-      @facebook.image_url = @defaultFacebook.excerpt_image.url
-      @facebook.site_type = @defaultFacebook.site_type
-      @facebook.site_name = @defaultFacebook.site_name
-      @facebook.admins = @defaultFacebook.admins      
-    end
-    @facebook.image_url = request.host_with_port+@facebook.image_url
-    @facebook.url = request.host_with_port+request.original_fullpath
     return @facebook
   end
 
-  def encodeURIComponent
-    @facebook_title_encode_URI = URI.escape(@facebook.title, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    @facebook_description_encode_URI = URI.escape(@facebook.description, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    @facebook_image_url_encode_URI = URI.escape(@facebook.image_url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    @facebook_site_type_encode_URI = URI.escape(@facebook.site_type, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    @facebook_url_encode_URI = URI.escape(@facebook.url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    @facebook_site_name_encode_URI = URI.escape(@facebook.site_name, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    @facebook_admins_encode_URI = URI.escape(@facebook.admins, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-  end
+  # def encodeURIComponent
+  #   @facebook_title_encode_URI = URI.escape(@facebook.title, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  #   @facebook_description_encode_URI = URI.escape(@facebook.description, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  #   @facebook_image_url_encode_URI = URI.escape(@facebook.image_url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  #   @facebook_site_type_encode_URI = URI.escape(@facebook.site_type, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  #   @facebook_url_encode_URI = URI.escape(@facebook.url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  #   @facebook_site_name_encode_URI = URI.escape(@facebook.site_name, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  #   @facebook_admins_encode_URI = URI.escape(@facebook.admins, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  # end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :application, "eyeplus"
-set :domain, "li406-49.members.linode.com"
+set :domain, "192.168.1.45"
 set :repository,  "git@github.com:SummersAdvertising/eyeplus.git"
-set :deploy_to, "/var/spool/RoR-Projects/eyeplus"
+set :deploy_to, "/home/apps/eyeplus"
 
 role :app, domain
 role :web, domain
@@ -16,9 +16,9 @@ set :scm_verbose, true
 set :use_sudo, false
 
 set :user, "apps"
-set :password, "1qaz2wsx"
-set :group, "webs"
-# set :group, "apps"
+# set :password, "1qaz2wsx"
+# set :group, "webs"
+set :group, "apps"
 
 default_environment["PATH"] = "/opt/ree/bin:/usr/local/bin:/usr/bin:/bin:/usr/games"
 
@@ -30,7 +30,7 @@ namespace :deploy do
 end
 
 desc "Create database.yml and asset packages for production"
-after("deploy:update_code") do
-  db_config = "#{shared_path}/config/database.yml.production"
-  run "cp #{db_config} #{release_path}/config/database.yml"
-end
+ after("deploy:finalize_update") do
+   db_config = "#{shared_path}/config/database.yml.production"
+   run "cp #{db_config} #{release_path}/config/database.yml"
+end 
