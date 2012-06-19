570e07f0cbe99d5ac9a74a0a3a09a8a4a67687b4
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-19 17:18:11 +0800

    capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ EyePlus::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.js )
   config.assets.precompile += %w( admin.css )
+  config.assets.precompile += %w( introduction.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
