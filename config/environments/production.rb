c226962fbc8d0a4cc929a315121a6145d1dd936a
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-20 19:06:16 +0800

    html-for-posts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,10 +47,10 @@ EyePlus::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.js )
-  config.assets.precompile += %w( boards.js )
   config.assets.precompile += %w( admin.css )
   config.assets.precompile += %w( boards.css )
   config.assets.precompile += %w( introduction.css )
+  config.assets.precompile += %w( posts.css )
   config.assets.precompile += %w( services.css )
   config.assets.precompile += %w( staff.css )
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
