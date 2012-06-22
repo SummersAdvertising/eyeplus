12811b93d0ba01f90671c9c9ce5fe63c7580ab02
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-22 14:06:55 +0800

    set up database seed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -82,7 +82,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 10
 
   # Setup a pepper to generate the encrypted password.
-  # config.pepper = "398030a9cdbb9bca0db60218a20e949f9da076e2e0ed7fc308dee878431cca66f6a6122a9099f1559b6b624d62d5f9292b0d5d9fe24a478713b6c5f3aa529675"
+  # config.pepper = "5c3c887db3f5ba2ada6d7ff052cf139daee60f4464457d449458eb622a82decc4d404241923dc8e0d396852fc709abb91ead79f5f1bfb40c253d8b327abc30f0"
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
@@ -125,6 +125,9 @@ Devise.setup do |config|
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
   # config.timeout_in = 30.minutes
+  
+  # If true, expires auth token on session timeout.
+  # config.expire_auth_token_on_timeout = false
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
@@ -181,9 +184,8 @@ Devise.setup do |config|
   # devise role declared in your routes (usually :user).
   # config.default_scope = :user
 
-  # Configure sign_out behavior.
-  # Sign_out action can be scoped (i.e. /users/sign_out affects only :user scope).
-  # The default is true, which means any logout action will sign out all active scopes.
+  # Set this configuration to false if you want /users/sign_out to sign out
+  # only the current scope. By default, Devise signs out all scopes.
   # config.sign_out_all_scopes = true
 
   # ==> Navigation configuration
@@ -213,4 +215,18 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
+
+  # ==> Mountable engine configurations
+  # When using Devise inside an engine, let's call it `MyEngine`, and this engine
+  # is mountable, there are some extra configurations to be taken into account.
+  # The following options are available, assuming the engine is mounted as:
+  #
+  #     mount MyEngine, at: "/my_engine"
+  #
+  # The router that invoked `devise_for`, in the example above, would be:
+  # config.router_name = :my_engine
+  #
+  # When using omniauth, Devise cannot automatically set Omniauth path,
+  # so you need to do it manually. For the users scope, it would be:
+  # config.omniauth_path_prefix = "/my_engine/users/auth"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,25 @@ normal_user = User.new(:email => "user@eyeplus.com.tw", :password => "123456",
   :password_confirmation => "123456")
 normal_user.save!
 
+super_admin = User.new(:email => "superadmin@eyeplus.com.tw", :password => "123456", 
+  :password_confirmation => "123456")
+super_admin.is_admin = true
+super_admin.is_super_admin = true
+super_admin.save!
+
+first_board = Board.new(:name => "保護與矯正兒童視力", :order_number => 1)
+first_board.save
+
+second_board = Board.new(:name => "近視與角膜塑形術", :order_number => 2)
+second_board.save
+
+third_board = Board.new(:name => "個人化配鏡指南", :order_number => 3)
+third_board.save
+
+forth_board = Board.new(:name => "對抗老花、白內障", :order_number => 4)
+forth_board.save
+
+
 default_facebook = DefaultFacebook.new(:title => "聚英眼科診所", :description => "聚英眼科診所", :site_type => "company", :url => "www.eyeplus.com.tw", :site_name => "聚英眼科診所", :admins => "1123456")
 default_facebook.user_id = 1
 default_facebook.save!
