7dd43b280b3c4782fa6bf387819da5fa16c19698
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-06-27 09:32:22 +0800

    checkout-email

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   attr_accessible :content, :email, :is_private, :title, :user_name
   belongs_to :user
   has_many :answers, :dependent => :destroy
-   validates :user_name, :presence => true
+  validates :user_name, :email, :presence => true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
