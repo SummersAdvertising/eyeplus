54064cbd798044080ad01feec688e58f5236c4c7
Elaphurus Liang <whoistoto@gmail.com>
2012-06-27 16:49:39 +0800

    images

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ActiveRecord::Base
   attr_accessible :content, :title, :excerpt_image_top, :excerpt_image_bottom
   belongs_to :board
   belongs_to :user
-  has_attached_file :excerpt_image_top, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  has_attached_file :excerpt_image_bottom, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :excerpt_image_top, :styles => { :medium => "565x565>", :thumb => "100x100>" }
+  has_attached_file :excerpt_image_bottom, :styles => { :medium => "565x565>", :thumb => "100x100>" }
   # default_scope :order => 'created_at DESC'
   scope :recent, :order => 'updated_at DESC'
   paginates_per 5
